5e2f18b4760e5918b0e098c4c3b8bd9223c68621
yurigurlie207 <adrienne.yao@gmail.com>
2021-03-22 21:55:42 -0400

    commit tree 377c08c66a710710633fe5e4998d589e7881c15c

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
 const Home = () => {
   return (
     <div>
@@ -9,7 +54,48 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+// Step 2. Changed to have router coordinate what is displayed
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+    <Route path="/" component={Home} />
+    {/* <Route exact path="/" component={Home} /> */}
+    {/* <Route path="/" render={() => <h1>Home!</h1>} /> */}
+    <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      </div>
+  </Router>),
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <Home />,
+//   document.getElementById('root')
+// );
